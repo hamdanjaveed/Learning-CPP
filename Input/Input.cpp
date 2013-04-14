7186343ede676e587c55f2825f92a1c96f104fa9
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-04-13 22:50:23 -0400

    1.1 Input [Frame]

--- Input/Input.cpp
+++ Input/Input.cpp
@@ -1,3 +1,4 @@
+// ask for the user's name, and then generate a framed greeting
 
 #include <iostream>
 #include <string>
@@ -11,8 +12,23 @@ int main() {
 	std::string name;
 	// read the name
 	std::cin >> name;
+
+	// build the message
+	const std::string greeting = "Hello, " + name + "!";
+
+	// build the second and fourth lines
+	const std::string spaces(greeting.size(), ' ');
+	const std::string secondAndFourth = "* " + spaces + " *";
+
+	// build the first and fifth lines of the output
+	const std::string firstAndFifth(secondAndFourth.size(), '*');
 	
-	// write a greeting
-	std::cout << "Hello, " << name << "!" << std::endl;
+	// write the greeting
+	std::cout << std::endl;
+	std::cout << firstAndFifth << std::endl;
+	std::cout << secondAndFourth << std::endl;
+	std::cout << "* " + greeting + " *" << std::endl;
+	std::cout << secondAndFourth << std::endl;
+	std::cout << firstAndFifth << std::endl;
 	return 0;
 }
