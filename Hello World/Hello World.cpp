8b015cc9961d5637ac035afd3d55418f78c1d512
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-04-13 22:19:00 -0400

    0.0 Hello World

--- Hello World/Hello World.cpp
+++ Hello World/Hello World.cpp
@@ -1,7 +1,11 @@
 
+// include iostream to be able to print to the console
 #include <iostream>
 
+// main function
 int main() {
+	// print to the console
 	std::cout << "Hello, world!" << std::endl;
+	// return 0
 	return 0;
 }
