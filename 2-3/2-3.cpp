b854293af04b0b64eb0c1f921b172f91581338e4
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-04-14 16:06:55 -0400

    2-3

---
+++ 2-3/2-3.cpp
@@ -0,0 +1,57 @@
+// 2-3 ask the user to specify padding
+
+#include <iostream>
+#include <string>
+
+// say that we're using the std namespace
+using namespace std;
+
+int main() {
+	// ask for the person's name
+	cout << "Please enter your name: ";
+
+	// read the name
+	// a variable to hold the user's name
+	string name;
+	// read the name
+	cin >> name;
+
+	// build the message
+	const string greeting = "Hello, " + name + "!";
+
+	// ask the user for the number of blank spaces around the greeting vertically
+	cout << "How many spaces vertically :";
+	int vPad = 0;
+	cin >> vPad;
+	// the number of blank spaces around the greeting horizontally
+	cout << "How many spaces horizontally :";
+	int hPad = 0;
+	cin >> hPad;
+
+	// the number of rows and columns to write
+	const int rows = vPad * 2 + 3;
+	const string::size_type columns = greeting.size() + hPad * 2 + 2;
+
+	// write a line to separate output
+	cout << endl;
+
+	// write the output
+	for (int r = 0; r < rows; r++) {
+		string::size_type c = 0;
+		while (c < columns) {
+			if (r == vPad + 1 && c == hPad + 1) {
+				cout << greeting;
+				c += greeting.size();
+			} else {
+				if (r == 0 || r == rows - 1 || c == 0 || c == columns - 1) {
+					cout << "*";
+				} else {
+					cout << " ";
+				}
+				c++;
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
