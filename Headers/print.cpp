25149136e2686532afdfa72aab2e642fd12dfb7d
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-04-19 22:27:52 -0400

    Headers

---
+++ Headers/print.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "print.h"
+
+using namespace std;
+
+void print(string message) {
+	cout << message << endl;
+}
+
+void printVector(vector<int> vect) {
+	cout << "[" << vect[0];
+	for (vector<int>::size_type i = 1; i < vect.size(); i++) {
+		cout << ", " << vect[i];
+	}
+	cout << "]" << endl;
+}
+
+void changeVector(vector<int>& vect) {
+	vect[0] = 71;
+}
---
+++ Headers/print.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_PRINT_H
+#define GUARD_PRINT_H
+
+#include <string>
+#include <vector>
+
+void print(std::string);
+void printVector(std::vector<int>);
+void changeVector(std::vector<int>&);
+
+#endif
